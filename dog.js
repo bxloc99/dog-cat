a7a46986ee443d54b92235f2abe07b43baff86f0
builoc <bxloc0807@gmail.com>
2021-06-26 08:15:26 +0700

    add package chalk and index

--- dog.js
+++ dog.js
@@ -1,3 +1,12 @@
-function dog(){
-	
-	}
+function dog(name){
+	this.name = name;
+	this.stomach = [];
+}
+
+dog.prototype.eat = function(cat){
+	this.stomach.push(cat);
+}
+dog.prototype.sayHi = function(){
+	console.log("hi i am a dog. My name is " + chalk.bule(this.name))
+}
+module.exports = dog;
